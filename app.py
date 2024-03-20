63660399f7251faebc9525434134981b4349d8e7
Andrew Schmal <116768411+AndrewSchmal@users.noreply.github.com>
2024-03-19 20:03:16 -0400

    Starting my task

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ def index():
 @app.get('/movies')
 def list_all_movies():
     # TODO: Feature 1
-    return render_template('list_all_movies.html', list_movies_active=True)
+    movies = movie_repository.get_all_movies()  # Assuming you have a method to fetch all movies
+    return render_template('list_all_movies.html', movies=movies, list_movies_active=True)
+
 
 
 @app.get('/movies/new')
