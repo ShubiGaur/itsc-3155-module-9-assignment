2c022b49b15088ee7fcc77d420d58531f5259bfe
AndrewSchmal <andrewschmal5@gmail.com>
2024-03-20 03:19:30 -0400

    updated list_all_movies.html

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def index():
 
 @app.get('/movies')
 def list_all_movies():
-    movies = movie_repository.get_all_movies()  # Fetch all movies from the repository
-    return render_template('list_all_movies.html', movies=movies, list_movies_active=True)
+    # TODO: Feature 1
+    return render_template('list_all_movies.html', list_movies_active=True)
 
 
 @app.route('/movies/new', methods=['GET', 'POST'])
